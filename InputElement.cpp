b10a83b67b68ccc39ea1c6f9094e7c0a02f0ef90
ABHISHEK KUMAR <170253904+abhishek202324@users.noreply.github.com>
2025-05-10 22:49:05 +0530

    Add files via upload

---
+++ InputElement.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[5];
+    cout<<"enter the array element: ";
+
+    for(int i=0;i<5;i++){
+        cin>>arr[i];
+
+    }
+    for(int i=0;i<5;i++){
+        cout<<arr[i]<<"  ";
+    }
+    cout<<endl;
+    cout<<"end of the code ";
+
+}

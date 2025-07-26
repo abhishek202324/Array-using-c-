721b763a74f32ecd232922d246547876926fbf36
ABHISHEK KUMAR <170253904+abhishek202324@users.noreply.github.com>
2025-07-26 23:04:02 +0530

    Add files via upload

--- MultipleFunctionString.cpp
+++ MultipleFunctionString.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<string>
+#include <cctype> 
 using namespace std;
 
 void inputStringSingleWord(string &str) {
@@ -104,12 +105,85 @@ void CountvowelConsonants(const string &str){
     cout<<"Total number of vovel in string : "<<vCount<<endl;
     cout<<"Total number of consonants  in string: "<<cCount<<endl;
 }
-void countdigitAlphaSpace(){
+void countdigitAlphaSpace(string &s){
+    int countAlpha=0;
+    int countdigit=0;
+    int countSpace=0;
+    int countSpecialChar=0;
+    int size=s.size();
+    for(char ch:s){
+        if(isalpha(ch))countAlpha++;
+        else if(isdigit(ch))countdigit++;
+       else if (isspace(ch))countSpace++;
+       else countSpecialChar++;
+    }
+    cout<<"The total number of alpha(like a,b,C,D) char in big sting is : "<<countAlpha<<endl;
+    cout<<"The total number of digits in big string is: "<<countdigit<<endl;
+    cout<<"The total number of space in big string is: "<<countSpace<<endl;
+    cout<<"The total number of special char in big string expect space is: "<<countSpecialChar<<endl;
 
 }
+void toggleCaseUpperTolowerViseVersa(string &s){
+    string temp = s;
+    for(int i = 0; i < temp.size(); i++){
+        if (islower(temp[i])) {
+            temp[i] = toupper(temp[i]);
+        }
+        else if (isupper(temp[i])) {           
+            temp[i] = tolower(temp[i]);
+        }
+       
+    }
+    
+    cout << "The string after toggle (upper↔lower): " << temp << endl;
+}
+unordered_map<char, int> countFrequency(const string &s) {
+    unordered_map<char, int> freq;
+    for (char ch : s) {
+        freq[ch]++;
+    }
+    cout << "Character Frequencies: " << endl;
+    for (auto pair : freq) {
+        cout << pair.first << " => " << pair.second << endl;
+    }
+    return freq; // return the map
+}
 
+void findFirstNonRepeating(const string &s, unordered_map<char, int> &freq) {
+    for (char ch : s) {
+        if (freq[ch] == 1) {
+            cout << "First non-repeating character is: " << ch << endl;
+            return;
+        }
+    }
+    cout << "No non-repeating character found." << endl;
+}
+void findFirstRepeating(const string &s,unordered_map<char,int> &freq){
+    for(char ch:s){
+        if(freq[ch]>1){
+            cout << "First repeating character is: " << ch << endl;
+            return;
+        }
+    }
+    cout << "No repeating character found." << endl;
 
-
+}
+void MaxFreqElement(const string &s,unordered_map<char,int> &freq){
+    int max=0;
+    char maxChar='\0';
+    
+    for (auto pair : freq) {
+        if(pair.second>max){
+            max=pair.second;
+            maxChar = pair.first;
+        }
+    }
+    if (maxChar != '\0') {
+        cout << "Most frequent character is: " << maxChar << " (Frequency: " << max << ")" << endl;
+    } else {
+        cout << "No characters found in the string." << endl;
+    }
+}
 
 int main() {
     string str;
@@ -133,5 +207,11 @@ int main() {
     lowertoupperFunction(str);
     uppertolowerfunction(str);
     CountvowelConsonants(str);
+    countdigitAlphaSpace(s);
+    toggleCaseUpperTolowerViseVersa(s);
+    unordered_map<char, int> freqMap = countFrequency(str); 
+    findFirstNonRepeating(str, freqMap);  
+    findFirstRepeating(str,freqMap);
+    MaxFreqElement(str,freqMap);
     return 0;
 }
